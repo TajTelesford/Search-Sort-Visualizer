fa93892874a7492c8baa3f8d4384124bb214b974
TajTelesford <TajTelesford@gmail.com>
2022-10-02 21:37:31 -0500

    Initial Commit

---
+++ Tutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tutorial
+//
+//  Created by Taj Telesford on 10/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
